47c31896d6b42f46ed33351145a88db5449893d7
Matthew Drumheller <MattDrummy@gmail.com>
2017-10-19 14:03:51 -0600

    added gin.log and logger

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
 import (
-  
+  "io"
+  "os"
+  "github.com/gin-gonic/gin"
+  "github.com/joho/godotenv"
 )
+
+func main()  {
+  gin.DisableConsoleColor()
+  f, _ := os.Create("gin.log")
+  gin.DefaultWriter = io.MultiWriter(f)
+  
+}
